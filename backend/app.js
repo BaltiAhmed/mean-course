f5518a2d1e069ceb1a0621bc74fd24cfa9b993e6
zainebhajji <hajjizaineb2019@gmail.com>
2020-10-26 14:40:57 +0100

    m

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
 
+app.use(function (req, res, next) {
+    /*var err = new Error('Not Found');
+     err.status = 404;
+     next(err);*/
 
-app.use('/api/posts', (req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+
+    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body
+    console.log(post)
+    res.status(201).json({ message: 'post added' })
+})
+
+app.get('/api/posts', (req, res, next) => {
     const posts = [
         { id: 'ghfghgdghs', title: 'first title', content: 'this is comming from the server' },
         { id: 'xjfhjfdb', title: 'second title', content: 'this is comming from the server' }
     ]
-    res.status(200).json({message:'fetch successfully',posts:posts})
+    res.status(200).json({ message: 'fetch successfully', posts: posts })
 })
 
 
--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,32 +1,36 @@
-import {Post} from './post.model'
+import { Post } from './post.model'
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http'
-import {Subject} from 'rxjs'
+import { HttpClient } from '@angular/common/http'
+import { Subject } from 'rxjs'
 
-@Injectable({providedIn:"root"})
-export class PostService{
-    private posts: Post[]=[];
+@Injectable({ providedIn: "root" })
+export class PostService {
+    private posts: Post[] = [];
     private postUpdated = new Subject<Post[]>()
 
-    constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) { }
 
-    getPost(){
-        this.http.get<{message:string, posts:Post[]}>('http://localhost:3000/api/posts')
-        .subscribe((postsData)=>{
-            this.posts= postsData.posts;
-            this.postUpdated.next([...this.posts])
-              
-        });
+    getPost() {
+        this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+            .subscribe((postsData) => {
+                this.posts = postsData.posts;
+                this.postUpdated.next([...this.posts])
+
+            });
     }
 
-    getPostUpdated(){
+    getPostUpdated() {
         return this.postUpdated.asObservable()
     }
 
-    addPost(title:string , content:string){
-        const post : Post={id:null,title:title,content:content}
-        this.posts.push(post)
-        this.postUpdated.next([...this.posts])
+    addPost(title: string, content: string) {
+        const post: Post = { id: null, title: title, content: content }
+        this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+            .subscribe((responseData) => {
+                console.log(responseData.message)
+                this.posts.push(post)
+                this.postUpdated.next([...this.posts])
+            })
 
     }
 }
